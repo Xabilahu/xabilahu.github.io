8ccef4ec1c8307b96105803012ab5258f2f4ca26
Xabier Lahuerta Vázquez <xabilahu99@hotmail.com>
2021-01-31 20:10:14 +0100

    Slide Traslation

--- logic.js
+++ logic.js
@@ -1,7 +1,17 @@
 var currectlyActiveCircle = undefined;
 
-function swapActiveCircle(element) {
-    currectlyActiveCircle = element;
+function swapActiveCircle(element, searchCircle) {
+    var id = "";
+    if (searchCircle == true) {
+        hideActiveCircle();
+        currectlyActiveCircle = document.querySelector("#navbar").children[element - 1];
+        id = document.getElementsByClassName("section-" + element)[0].id;
+        showActiveCircle();
+    } else {
+        currectlyActiveCircle = element;
+        id = element.getAttribute("target-section");
+    }
+    document.querySelector("#" + id).scrollIntoView({behavior: "smooth", block: "end"});
 }
 
 function hideActiveCircle() {
@@ -23,7 +33,8 @@ function generateNavbarDivs() {
     var first = true;
     for (var d of document.querySelectorAll("div[class^='section-']")) {
         var div = document.createElement("div");
-        div.setAttribute("onclick", "swapActiveCircle(this);")
+        div.setAttribute("onclick", "swapActiveCircle(this, false);");
+        div.setAttribute("target-section", d.id);
         div.classList.add("circle");
         if (first) div.classList.add("active");
         navbar.appendChild(div);
